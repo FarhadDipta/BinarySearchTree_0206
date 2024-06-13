5a082fb956fefdf6956cf470493579ef54b420a7
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-06-13 15:56:47 +0700

    menyelesaikan class BinaryTree

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -82,6 +82,36 @@ public:
             inorder(ptr->rightchild);
         }
     }
+
+    void preorder (Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    void postorder (Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
 
 
